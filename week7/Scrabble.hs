056ac552cb75973804dfaf216338bedb16c3d07c
Hugo Ludemann <hugo.ludemann@gmail.com>
2022-12-15 09:23:10 +1100

    modified scrabble.hs

--- week7/Scrabble.hs
+++ week7/Scrabble.hs
@@ -1,4 +1,16 @@
-module Scrabble
+module Scrabble where
+
+import Data.List (find)
+import Data.Maybe (fromMaybe)
+
+scoreData :: [(String, Score)]
+scoreData = [("aeilnordstu", Score 1),
+             ("dg", Score 2),
+             ("bcmp", Score 3),
+             ("fhvwy", Score 4),
+             ("k", Score 5),
+             ("xj", Score 8),
+             ("qz", Score 10)]
 
 newtype Score = Score Int
   deriving (Eq, Ord, Show, Num)
@@ -12,7 +24,6 @@ instance Semigroup Score where
 instance Monoid Score where
   mempty = Score 0
 
-("aeilnordstu", 1), ("dg", 2), ("bcmp", 3)
-
 score :: Char -> Score
-
+score c = fromMaybe 0 (snd <$> x) where
+  x = find (\(chars, value) -> elem c chars) scoreData
