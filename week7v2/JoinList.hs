95161de112bddf16e3eaf4d8a4666a1300267d6a
hlud6646 <hugo.ludemann@gmail.com>
2022-12-20 19:42:42 +1100

    w7v2

--- week7v2/JoinList.hs
+++ week7v2/JoinList.hs
@@ -17,9 +17,20 @@ tag Empty = mempty
 tag Single m _   = m
 tag Append m _ _ = m
 
+-- If the annotation represents the number of elements in a tree then we have 
+-- an index. If we are after the i-th element, see if the left tree contains 
+-- at least that many elements. It it does then recurse on that subtree. Other
+-- -wise zoom in on the right tree.
 indexJ :: (Sized b, Monoid b) => Int -> JoinlList b a -> Maybe a
-indexJ 1 x@Single _ _ = x
+indexJ 0 (Single _ a)     = Just a
 indexj i Append a l r
-  | i <= tag l = indexJ i l
-  | otherwise = indexJ (a - tag l) r
-indexj _ _ = Nothing
+  | i <= getSize $ size l = indexJ i l
+  | otherwise             = indexJ (a - getSize $ size l) r
+indexj _ _                = Nothing
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n (Append a l r) 
+  | n <= getSize $ size l = (dropJ n l) +++ r
+  | otherwise             = dropJ (n - getSize $ size l) r
+dropJ _ _                 = Empty
+
