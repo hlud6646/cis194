b72e54149162da240e108c5bc2c1fa2850444815
hlud6646 <hugo.ludemann@gmail.com>
2022-12-22 01:54:16 +1100

    w8

--- week8/Party.hs
+++ week8/Party.hs
@@ -1,7 +1,10 @@
 module Party where
 
+import System.IO
+
 import Employee
 import Data.Tree
+import Data.List (sort)
 
 -- Assuming that the new guest is not already in the list.
 glCons :: Employee -> GuestList -> GuestList
@@ -22,3 +25,24 @@ treeFold :: b               -- The initial value of the accumulator.
         -> b                -- Returns the final accumulator.
 treeFold e f (Node a tas) =  f a (map (treeFold e f) tas)
 
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss pairs = (bestWithBoss boss pairs, bestWithoutBoss pairs) where
+    bestWithBoss lackey []  = glCons lackey mempty                      -- A bottom rung employee goes on a singleton GL.
+    bestWithBoss boss pairs = glCons boss (maximum (map snd pairs))     -- Only add Bob to lists which don't contain a direct subordinate.
+    bestWithoutBoss []  = mempty                                        -- The employee has no subordinates, but this function insists that they still don't go. So an empty GL.
+    bestWithoutBoss pairs   = maximum (map fst pairs ++ map snd pairs)  -- Best list out of everything available. Bob's not coming, so no fun detraction possible.
+
+maxFun :: Tree Employee -> GuestList
+maxFun boss = (uncurry max) (treeFold (mempty, mempty) nextLevel boss)
+
+totalFun :: GuestList -> Fun
+totalFun (GL _ n) = n
+empNames :: GuestList -> [String]
+empNames (GL es _) = sort $ map empName es
+
+main = do
+    inpStr <- readFile "company.txt"
+    let company = read inpStr :: Tree Employee
+        gl      = maxFun company
+    putStrLn . show $ totalFun gl
+    mapM_ putStrLn (empNames gl)
